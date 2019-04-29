4c45e5f757ad1903be67132455f803c069b943d0
orangeos <36617362+orangeos@users.noreply.github.com>
2019-04-29 13:38:38 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,31 @@
+print("IF IT DOESN'T WORKING READ README")
+print("To learn the commands type hlp()")
+
+def calc():
+	a = input()
+	b = input()
+	print("The sum is:", a + b)
+def mu():
+        c = input()
+        d = input()
+        print("The sum is:", c * d)
+def ext():
+        print("CLOSING")
+        exit()
+def hlp():
+        print("To subtract two numbers type sub()")
+        print("To add two numbers type calc()")
+        print("To multiply two numbers type mu()")
+        print("To divide two numbers type div()")
+        print("To exit type ext()")
+def div():
+        s = input()
+        d = input()
+        print("The sum is:", s / d)
+def sub():
+        j = input()
+        h = input()
+        print("The sum is:", j - h)
+        
+
+
